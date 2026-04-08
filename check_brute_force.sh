3b709c0d6146a21950c4d5da292a56e335bd13c7
teyt0eyz <upperxyz@gmail.com>
2026-04-08 15:23:56 +0700

    Delete check_brute_force.sh

--- check_brute_force.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-logfile=/home/admin/Lab2/syslog_test.log
-grep "Failed password" "$logfile" | awk '{print $11}' | sort |uniq -c  > report.txt
-
-awk '$1 > 20 {print "ALERT: " $2 }' report.txt 
-
-
